6dfa074dcd4b13495ff35709e3c21c5a29ab62c7
ReinaldoRoque <reinaldo.roque91@gmail.com>
2026-02-19 16:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            // return new Window(new AppShell());
+
+            return new Window(new Views.ListaProduto());
+
+        }
+    }
+}
