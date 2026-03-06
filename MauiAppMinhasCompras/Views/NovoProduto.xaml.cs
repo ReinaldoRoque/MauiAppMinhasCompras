1fd89cee2521f266f72f940947cc28c3a3e35f38
ReinaldoRoque <reinaldo.roque91@gmail.com>
2026-03-06 15:00:17 -0300

    agenda 3 - criação do insert, tornando a classe disponivel para o app

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -13,16 +13,16 @@ public partial class NovoProduto : ContentPage
     private async void ToolbarItem_Clicked(object sender, EventArgs e)
     {
 		try
-		{
-			Produto p = new Produto
-			{
+		{	
+			Produto p = new Produto // PRENCHENDO A MODEL DE PRODUTO
+            {
 				Descricao = txt_descricao.Text,
 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
 				Preco = Convert.ToDouble(txt_preco.Text)
 			};
 
-			await App.Db.Insert(p);
-			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+			await App.Db.Insert(p); // CRIANDO O INSERT 
+			await DisplayAlert("Sucesso!", "Registro Inserido", "OK"); // AVISANDO O USUARIO QUE DEU CERTO
 
 		}catch (Exception ex)
 		{
