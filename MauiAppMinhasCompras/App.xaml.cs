f7c1f761f886cb1885d9b10031947547b84dab95
ReinaldoRoque <reinaldo.roque91@gmail.com>
2026-03-06 14:45:52 -0300

    agenda 3 - criação do insert, tornando a classe disponivel para o app

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,34 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        /* 
+        AVISANDO QUE VAMOS TRABALHAR COM SQLYTE
+        Nele vamos tornar a classe disponivel para o app
+        */
+        static SQLiteDatabaseHelper _db; // CAMPO
+
+        public static SQLiteDatabaseHelper Db // PROPRIEDADE DE LEITURA
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.bd3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+
+            }
+        }
+
         public App()
         {
             InitializeComponent();
@@ -12,7 +39,7 @@
         {
             // return new Window(new AppShell());
 
-            return new Window(new Views.ListaProduto());
+            return new Window(new NavigationPage(new Views.ListaProduto()));
 
         }
     }
